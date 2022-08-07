17191f307413d134c8366a9acc4f9536209a3575
andreili <andreil499@gmail.com>
2022-08-07 23:57:19 +0200

    Remove generated files.

--- vrf/obj_dir/verilated.d
+++
@@ -1,9 +0,0 @@
-verilated.o: C:/msys64/mingw64/share/verilator/include/verilated.cpp \
- C:/msys64/mingw64/share/verilator/include/verilatedos.h \
- C:/msys64/mingw64/share/verilator/include/verilated_imp.h \
- C:/msys64/mingw64/share/verilator/include/verilated.h \
- C:/msys64/mingw64/share/verilator/include/verilated_config.h \
- C:/msys64/mingw64/share/verilator/include/verilated_types.h \
- C:/msys64/mingw64/share/verilator/include/verilated_funcs.h \
- C:/msys64/mingw64/share/verilator/include/verilated_syms.h \
- C:/msys64/mingw64/share/verilator/include/verilated_sym_props.h
